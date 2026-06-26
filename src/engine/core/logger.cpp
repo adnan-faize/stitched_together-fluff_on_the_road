0c43efef3c399967080b674d12a309e9c23b9dda
Adnan Faize <adnanfaize@gmail.com>
2026-06-26 18:38:53 +0200

    writing the logger

--- src/engine/core/logger.cpp
+++ src/engine/core/logger.cpp
@@ -1,8 +1,12 @@
 /**
+ * @ file logger.hpp
  *
+ * @ brief ...
+ *
+ * @ author Adnan FAIZE <>
  */
 
-#include "engine/core/logger.h"
+#include "engine/core/logger.hpp"
 #include <iostream>
 #include <filesystem>
 #include <chrono>
@@ -13,12 +17,21 @@ void Logger::Init(const std::string& filename) {
     m_File.open(filename, std::ios::app);
 }
 
+void Logger::Shutdown() {
+    std::lock_guard<std::mutex> lock(m_Mutex);
+
+    if (m_File.is_open()) {
+        m_File.flush();
+        m_File.close();
+    }
+}
+
 void Logger::Log(LogLevel level, const std::string& message) {
     std::lock_guard<std::mutex> lock(m_Mutex);
 
     std::string prefix = "TODO";
 
-    std::cout << prefix << message << std::endl;
+    std::cout << prefix << message << std::endl; // TODO : Add datetime
 
     if (m_File.is_open()) {
         m_File << prefix << message << std::endl;
@@ -28,10 +41,18 @@ void Logger::Log(LogLevel level, const std::string& message) {
 }
 
 void Logger::CheckFileSize() {
-    
+    if (std::filesystem::exists(m_Filename) && std::filesystem::file_size(m_Filename) > MAX_FILE_SIZE) {
+        CompressLogFile();
+    }
 }
 
 void Logger::CompressLogFile() {
     m_File.close();
-    
+
+    // TODO : Compress the file
+
+    std::string archiveName = m_FileName + "." + std::to_string(std::chrono::system_clock::now().time_since_epoch().count()) + ".log";
+    std::filesystem::rename(m_Filename, archiveName);
+
+    m_File.open(m_Filename, std::ios::app);
 }
